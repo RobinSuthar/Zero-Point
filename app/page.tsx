4cd1edcde6c8f02048fa893ae386843511f81d6f
Aditya Suthar <adityabishnoi576@gmail.com>
2026-02-23 18:31:04 +0530

    TRIAL

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-          <div>
-            ADITYA
+          <div className = "bada_box">
+            <div className = "chota_box">
+              <h1>
+                Project Zero Point
+              </h1>
+            </div>
           </div>
   );
 }
