e6b81da4ffefbb4add322c9648680f376b0baa63
Aditya Suthar <adityabishnoi576@gmail.com>
2026-02-23 18:52:31 +0530

    added SIGN IN SIGN UP things (not buttons)

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,19 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-          <div className = "bada_box">
-            <div className = "chota_box">
-              <h1>
-                Project Zero Point
-              </h1>
+          <div className = "header">
+            <h1>
+              PROJECT ZERO POINT
+            </h1>
+
+            <br />
+
+            <div className = "box">
+              <h2> LOGIN </h2>
+            </div>
+
+            <div className = "box">
+              <h2> SIGN UP </h2>
             </div>
           </div>
   );
